a9b22e7ea133351e628008902f85cca062ad5fdb
lucifer116664 <lucifer116664@gmail.com>
2023-08-01 11:07:25 +0300

    Created utils and properties

---
+++ src/server/utils/ConnectionManager.java
@@ -0,0 +1,19 @@
+package server.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public final class ConnectionManager {
+    private static final String URL = PropertiesUtil.getProperty("db.url");
+    private static final String USERNAME = PropertiesUtil.getProperty("db.username");
+    private static final String PASSWORD = PropertiesUtil.getProperty("db.password");
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
